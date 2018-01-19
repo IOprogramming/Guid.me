45d824317cc28f04bfe6ed89007b263c057747b7
Ahmed Magdy Abd-alzaher <ahmed.magdy.abd.alzaher@gmail.com>
2018-01-19 21:09:56 +0200

    Changes Have Done By Ahmed Magdy

---
+++ app/src/main/java/com/me/guid/Places/Places.java
@@ -0,0 +1,23 @@
+package com.me.guid.Places;
+
+/**
+ * Created by Ahmed Magdy on 1/19/2018.
+ */
+
+public class Places {
+    public static final String[] Egypt = new String[] {
+            "Karnak",
+            "Great Sphinx of Giza",
+            "Abu Simbel temples",
+            "Luxor Temple",
+            "Egyptian Museum",
+            "Valley of the Kings",
+            "Mount Sinai",
+            "Colossi of Memnon",
+            "Lake Nasser",
+            "Aswan Dam",
+            "Saint Catherine's Monastery",
+            "Pyramids",
+            "Cairo Citadel"
+    };
+}
